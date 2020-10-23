e180ac9fa18d1ecc0f50118c1da7ca61707cc212
hansa <hansajayathilaka@yahoo.com>
2020-10-23 20:10:03 +0530

    Add Login Page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
             TDSVal: 0,
             FlowSpeed: 0,
             WaterLevel: 0,
+            login: false,
         };
     }
 
@@ -37,24 +38,66 @@ class App extends Component {
     }
 
     render() {
-        return (
-            <div className="container">
-                <h1 style={{ textAlign: "center" }}>River Protecter</h1>
-                    <div className="row" id="tds">
-                        <label className="col-md-3">TDS Reading</label>
-                        <span className="col-md-3">{this.state.TDSVal}</span>
-                    </div>
-                    <div className="row" id="flow-speed">
-                        <label className="col-md-3">Water Flow Speed Reading</label>
-                        <span className="col-md-3">{this.state.FlowSpeed}</span>
-                    </div>
-                    <div className="row" id="water-level">
-                        <label className="col-md-3">Water Level Reading</label>
-                        <span className="col-md-3">{this.state.WaterLevel}</span>
-                    </div>
-            </div>
+        const labelStyle3={
+            color:'red',
+            marginTop:"60px",
+            fontSize:'70px',
 
-        );
+        };
+        const labelStyle2={
+            color:'white',
+            marginTop:"60px",
+            fontSize:'50px',
+        };
+        const labelStyle={
+            color:'red',
+            marginTop:"60px",
+            fontSize:'50px',
+
+        };
+
+        const home = <div className="container">
+            <h1 style={{ textAlign: "center",marginTop:"50px"}}>River Protecter </h1>
+            <div className="row" id="tds">
+                <label className="col-md-6"style={labelStyle2}>TDS Reading</label>
+                <span className="col-md-3"style={labelStyle}>{this.state.TDSVal}</span>
+            </div>
+            <div className="row" id="flow-speed">
+                <label className="col-md-6"style={labelStyle2}>Water Flow Speed Reading</label>
+                <span className="col-md-3"style={labelStyle3}>{this.state.FlowSpeed}</span>
+            </div>
+            <div className="row" id="water-level">
+                <label className="col-md-6"style={labelStyle2}>Water Level Reading</label>
+                <span className="col-md-3"style={labelStyle}>{this.state.WaterLevel}</span>
+            </div>
+        </div>;
+
+        const login = <div className="login-box">
+            <form>
+                <h1>Login</h1>
+                <div className="textbox">
+                    <i className="fas fa-user"></i>
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className="textbox">
+                    <i className="fas fa-lock"></i>
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button type="button" className="btn" onClick={(evt) => {
+                    this.setState({ ...this.state, login: !this.state.login });
+                }
+                }>Sign in</button>
+            </form>
+        </div>;
+
+        const data = () => {
+            if (this.state.login)
+                return home;
+            else
+                return login;
+        };
+
+        return (data());
     }
 }
 
